63d4d9d3bfefbbfe5f0e0a6f62bffb022a2509ac
Stav Davis <stav.davis@gmail.com>
2017-12-04 13:28:57 -0800

    added a vaccineRouter file

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 //Mongoose internally uses a promise-like object,
 // but its better to make Mongoose use built in es6 promises
 mongoose.Promise = global.Promise;
+app.use(morgan('common'));
 // config.js is where we control constants for entire
 // app like PORT and DATABASE_URL
 const {PORT, DATABASE_URL} = require('./config');
@@ -13,107 +15,14 @@ const Vaccine = require('./src/models/vaccine');
 app.use(express.static('./src/public')); //to serve static index.html file
 app.use(bodyParser.json());
 
-// GET requests to /vaccines
-app.get('/vaccines', (req, res) => {
-  Vaccine
-    .find()
-    .then(vaccines => {
-      res.json({
-        vaccines: vaccines.map(
-          (vaccine) => vaccine.apiRepr())
-      });
-    })
-    .catch(
-      err => {
-        console.error(err);
-        res.status(500).json({message: 'Internal server error'});
-    });
-});
+///////ENDPOINT ROUTING MANAGEMENT SECTION - START
+const vaccinesRouter = require('./src/routers/vaccinesRouter');
+app.use('/vaccines', vaccinesRouter);
 
-// can also request by patientID
-app.get('/vaccines/:patientId', (req, res) => {
-  Vaccine
-    .find({patientId: req.params.patientId})
-    .then(vaccine =>res.json(vaccine.apiRepr()))
-    .catch(err => {
-      console.error(err);
-        res.status(500).json({message: 'Internal server error'})
-    });
-});
+///////ENDPOINT ROUTING MANAGEMENT SECTION- END
 
-//ADDING A POST ENDPOINT
-app.post('/vaccines', (req, res) => {
-  const requiredFields = ['vaccineName', 'vaccineStatus', 'patientId', 'relatedDiseases'];
-  for (let i=0; i<requiredFields.length; i++) {
-    const field = requiredFields[i];
-    if (!(field in req.body)) {
-      const message = `Missing \`${field}\` in request body`
-      console.error(message);
-      return res.status(400).send(message);
-    }
-  }
-  Vaccine
-    .create({
-      vaccineName: req.body.vaccineName,
-      vaccineStatus: req.body.vaccineStatus,
-      patientName: req.body.patientName,
-      patientId: req.body.patientId,
-      relatedDiseases: req.body.relatedDiseases,
-      dueDate: req.body.dueDate,
-      doneDate: req.body.doneDate})
-    .then(
-      restaurant => res.status(201).json(restaurant.apiRepr()))
-    .catch(err => {
-      console.error(err);
-      res.status(500).json({message: 'Internal server error'});
-    });
-});
-
-//ADDING PUT ENDPOINT
-app.put('/vaccines/:id', (req, res) => {
-  // ensure that the id in the request path and the one in request body match
-  if (!(req.params.id && req.body.id && req.params.id === req.body.id)) {
-    const message = (
-      `Request path id (${req.params.id}) and request body id ` +
-      `(${req.body.id}) must match`);
-    console.error(message);
-    res.status(400).json({message: message});
-  }
-  // we only support a subset of fields being updateable.
-  // if the user sent over any of the updatableFields, we udpate those values
-  // in document
-  const toUpdate = {};
-  const updateableFields = ['vaccineStatus', 'dueDate', 'doneDate'];
-
-  updateableFields.forEach(field => {
-    if (field in req.body) {
-      toUpdate[field] = req.body[field];
-    }
-  });
-  Vaccine
-    // all key/value pairs in toUpdate will be updated -- that's what `$set` does
-    .findByIdAndUpdate(req.params.id, {$set: toUpdate})
-    .then(vaccine => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-});
-
-//ADDING DELETE ENDPOING
-app.delete('/vaccines/:id', (req, res) => {
-  Vaccine
-    .findByIdAndRemove(req.params.id)
-    .then(vaccine => res.status(204).end())
-    .catch(err => res.status(500).json({message: 'Internal server error'}));
-});
-
-// catch-all endpoint if client makes request to non-existent endpoint
-app.use('*', function(req, res) {
-  res.status(404).json({message: 'Not Found'});
-});
-
-//CREATING RUNSERVER AND CLOSESERVER (FOR TESTING)
-// closeServer needs access to a server object, but that only
-// gets created when `runServer` runs, so we declare `server` here
-// and then assign a value to it in run
+///////CREATING RUNSERVER AND CLOSESERVER (FOR TESTING) SECTION - START
+// closeServer needs a server object (assigned in `runServer`)
 let server;
 
 // this function connects to our database, then starts the server
@@ -156,5 +65,6 @@ function closeServer() {
 if (require.main === module) {
   runServer().catch(err => console.error(err));
 };
+///////CREATING RUNSERVER AND CLOSESERVER (FOR TESTING) SECTION - START
 
 module.exports = {app, runServer, closeServer};
