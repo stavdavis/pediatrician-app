ecbb694a903173dc6d5ab1959c7cbc2377bb8afb
Stav Davis <stav.davis@gmail.com>
2017-12-03 21:19:33 -0800

    added mLab DATABASE URL to heroku

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 exports.DATABASE_URL = process.env.DATABASE_URL ||
                        global.DATABASE_URL ||
-                      'mongodb://localhost/pediatrician-app';
+                      'mongodb://localhost/pediatrician-app'; 
 exports.TEST_DATABASE_URL = (
 	process.env.TEST_DATABASE_URL ||
 	'mongodb://localhost/test-pediatrician-app');
